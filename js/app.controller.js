f812f621fd947490c95f06d0e584fdb8341d67ff
Efrat Stoztki <efratvaik@gmail.com>
2022-11-02 19:31:04 +0200

    continue 1.3

--- js/app.controller.js
+++ js/app.controller.js
@@ -7,6 +7,9 @@ window.onPanTo = onPanTo
 window.onGetLocs = onGetLocs
 window.onGetUserPos = onGetUserPos
 window.onSearchPlace = onSearchPlace
+window.onClickAddPlace = onClickAddPlace
+
+
 
 function onInit() {
     mapService.initMap()
@@ -29,6 +32,11 @@ function onInit() {
         })
         .catch(() => console.log('Error: cannot init map'))
         renderLocs()
+        mapService.getNameOfPos()
+            .then(res => {
+                console.log(res);
+                // document.querySelector('.try').innerText(res) // למה זה אומר שזה לא מאותחל?
+            })
 }
 
 function renderLocs() {
@@ -48,10 +56,26 @@ function renderLocs() {
     document.querySelector('.my-locations').innerHTML = strHtml
 }
 
+function onClickAddPlace() {
+
+    var gCurrLoc = locService.getCurrLock()
+    var name = prompt('Enter the name of the loc')
+
+    locService.addLocToLocsArry(name,   gCurrLoc.lat,  gCurrLoc.lng)
+    renderLocs()
+    document.querySelector('.add-place').classList.add('hide')
+}
 
 function onEddPlace(ev) {
     var lat = ev.latLng.lat()
     var lng = ev.latLng.lng()
+
+    //פה אני אתפוס את הכפתור ואשנה את הדיספליישלו, 
+    document.querySelector('.add-place').classList.remove('hide')
+    //צריך לעשות את המיקום הנוכחי למיקום הזה
+    locService.setCurrLock({ lat, lng })
+
+
     mapService.addMarker({ lat, lng })
 }
 
